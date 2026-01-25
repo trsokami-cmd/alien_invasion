c3602cf4772bac73f153cda8b47c3a425b9708b2
trsokami <trsokami@gmail.com>
2026-01-25 16:58:35 +0800

    控制帧率

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ class AlienInvasion:
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
@@ -21,6 +22,7 @@ class AlienInvasion:
 
                 # 让最近绘制的屏幕可见
                 pygame.display.flip()
+                self.clock.tick(60)
 
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
