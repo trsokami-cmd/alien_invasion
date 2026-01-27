03a7a9e06e2b55c52ca869ccc1a75dc1c5895891
trsokami <trsokami@gmail.com>
2026-01-27 19:41:42 +0800

    创建_update_bullets()方法

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,14 +31,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # 删除已消失的子弹
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-
-            self._upgrade_screen()
+            self._update_bullets()
+            self._update_screen()
             self.clock.tick(60)
 
     def _check_events(self):
@@ -77,7 +71,17 @@ class AlienInvasion:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
 
-    def _upgrade_screen(self):
+    def _update_bullets(self):
+        """更新子弹的位置并删除已消失的子弹"""
+        # 更新子弹的位置
+        self.bullets.update()
+
+        # 删除已消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
         # 填充背景，使用settings里的背景颜色
         self.screen.fill(self.settings.bg_color)
