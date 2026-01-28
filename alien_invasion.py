a2e73a6cc40ca480aba49dcb3b04ba7c3ce8d67d
trsokami <trsokami@gmail.com>
2026-01-28 16:59:25 +0800

    重构_create_fleet()

--- alien_invasion.py
+++ alien_invasion.py
@@ -94,11 +94,15 @@ class AlienInvasion:
 
         current_x = alien_width
         while current_x < (self.settings.screen_width - 2 * alien_width):
-            new_alien = Alien(self)
-            new_alien.x = current_x
-            new_alien.rect.x = new_alien.x
-            self.aliens.add(new_alien)
+            self._create_alien(current_x)
             current_x += 2 * alien_width
+
+    def _create_alien(self, x_position):
+        """创建一个外星人并将其放在当前行中"""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        self.aliens.add(new_alien)
 
 
     def _update_screen(self):
