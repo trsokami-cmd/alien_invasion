b3380b04a604577f7fc814e5e040ca6ea8c7948a
trsokami <trsokami@gmail.com>
2026-01-30 19:36:28 +0800

    创建表示外星舰队移动方向的设置

--- settings.py
+++ settings.py
@@ -20,3 +20,6 @@ class Settings:
 
         # 外星人设置
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction为1表示向右移动，为-1表示向左移动
+        self.fleet_direction = 1
