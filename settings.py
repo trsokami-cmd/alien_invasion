637f7e26419ba1844075eb61b71a90db359a45d8
trsokami <trsokami@gmail.com>
2026-01-31 16:04:09 +0800

    恢复设置

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ class Settings:
         self.bullets_allowed = 3
 
         # 外星人设置
-        self.alien_speed = 3.0
-        self.fleet_drop_speed = 50
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
         # fleet_direction为1表示向右移动，为-1表示向左移动
         self.fleet_direction = 1
